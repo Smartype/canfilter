27a529d888ad629108ee08b6ecc74454e3091e1e
Eric <smartype@gmail.com>
2021-09-21 17:38:30 +0800

    FIFO1 IRQ

--- Core/Inc/stm32f1xx_it.h
+++ Core/Inc/stm32f1xx_it.h
@@ -58,11 +58,13 @@ void PendSV_Handler(void);
 void SysTick_Handler(void);
 void CAN1_TX_IRQHandler(void);
 void CAN1_RX0_IRQHandler(void);
+void CAN1_RX1_IRQHandler(void);
 void CAN1_SCE_IRQHandler(void);
 void TIM6_IRQHandler(void);
 void TIM7_IRQHandler(void);
 void CAN2_TX_IRQHandler(void);
 void CAN2_RX0_IRQHandler(void);
+void CAN2_RX1_IRQHandler(void);
 void CAN2_SCE_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
