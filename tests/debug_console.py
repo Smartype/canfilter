4a81a494f59a59706297771b56802900db276922
Comma Device <device@comma.ai>
2022-01-22 18:59:45 +0800

    update scripts

--- tests/debug_console.py
+++ tests/debug_console.py
@@ -18,10 +18,21 @@ def main():
       for m in c:
         msg_id, x, dat, bus = m
         if msg_id == 0x2A8:
-            pass
+          crash_state = dat[0] >> 6
+          can2_st = (dat[0] >> 3) & 0x7
+          can2_st = dat[0] & 0x7
+          ver = dat[1:5].hex()
+          uptime, = struct.unpack('>H', dat[5:7])
+          acc_timeout = dat[7] & 0x1f
+          aeb_to = ((dat[7] >> 5) & 1)
+          pc_to = ((dat[7] >> 6) & 1)
+          pc2_to = ((dat[7] >> 7) & 1)
+          print(f"[HB] crash_state: {crash_state}, version: {ver}, can1_status: {can1_st}, can2_status: {can2_st}, uptime: {uptime}, aeb_to: {aeb_to}, pc_to: {pc_to}, pc2_to: {pc2_to}")
+
         elif msg_id == 0x2A9:
           rx_errs, send_errs, rx_cnt, tx_cnt, txd_cnt, err_cnt, csum_err, overflow_cnt = struct.unpack('BBBBBBBB', dat)
           print(f"[ST] rx errs: {rx_errs}, send_errs: {send_errs}, rx_cnt: {rx_cnt}, tx_cnt: {tx_cnt}, txd_cnt: {txd_cnt}, err_cnt: {err_cnt}, scum_err: {csum_err}, overflow_cnt: {overflow_cnt}")
+
         elif msg_id == 0x2AA:
           sys.stdout.write('[LG] ' + dat.decode('ascii'))
 
