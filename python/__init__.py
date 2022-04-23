2c6b58db64cd8382d4654a002c4e6ebb1f831cb6
Eric <smartype@gmail.com>
2022-04-23 17:17:25 +0800

    update python

--- python/__init__.py
+++ python/__init__.py
@@ -1,9 +1,9 @@
 import signal
+import struct
 import sys
 import os
 import time
 from panda import Panda
-from panda import CanHandle
 
 __version__ = '0.0.1'
 
@@ -21,14 +21,12 @@ FEATURE_LOCKSPEED = 4
 FEATURE_FAKELEAD  = 8
 FEATURE_PASSTHRU  = 16
 
-class CanFilterHandle(object):
-
-  def __init__(self, p, bus):
-    self.p = p
-    self.bus = bus
+class BootLoaderHandle(object):
+  def __init__(self, panda):
+    self.panda = panda
 
   def transact(self, dat):
-    self.p.isotp_send(CAN_FILTER_ISOTP_RX, dat, self.bus, recvaddr=CAN_FILTER_ISOTP_TX)
+    self.panda.isotp_send(1, dat, 0, recvaddr=2)
 
     def _handle_timeout(signum, frame):
       # will happen on reset
@@ -37,50 +35,88 @@ class CanFilterHandle(object):
     signal.signal(signal.SIGALRM, _handle_timeout)
     signal.alarm(1)
     try:
-      ret = self.p.isotp_recv(CAN_FILTER_ISOTP_TX, self.bus, sendaddr=CAN_FILTER_ISOTP_RX)
+      ret = self.panda.isotp_recv(2, 0, sendaddr=1, subaddr=None, bs=1, st=20)
+    finally:
+      signal.alarm(0)
+
+    return ret
+
+  def controlWrite(self, request_type, request, value, index, data, timeout=0):
+    # ignore data in reply, panda doesn't use it
+    return self.controlRead(request_type, request, value, index, 0, timeout)
+
+  def controlRead(self, request_type, request, value, index, length, timeout=0):
+    dat = struct.pack("HHBBHHH", 0, 0, request_type, request, value, index, length)
+    return self.transact(dat)
+
+  def bulkWrite(self, endpoint, data, timeout=0):
+    if len(data) > 0x10:
+      raise ValueError("Data must not be longer than 0x10")
+    dat = struct.pack("HH", endpoint, len(data)) + data
+    return self.transact(dat)
+
+  def bulkRead(self, endpoint, length, timeout=0):
+    dat = struct.pack("HH", endpoint, 0)
+    return self.transact(dat)
+
+class FilterHandle(object):
+  def __init__(self, panda):
+    self.panda = panda
+
+  def transact(self, dat):
+    self.panda.isotp_send(CAN_FILTER_ISOTP_RX, dat, 0, recvaddr=CAN_FILTER_ISOTP_TX)
+
+    def _handle_timeout(signum, frame):
+      # will happen on reset
+      raise Exception("timeout")
+
+    signal.signal(signal.SIGALRM, _handle_timeout)
+    signal.alarm(1)
+    try:
+      ret = self.panda.isotp_recv(CAN_FILTER_ISOTP_TX, 0, sendaddr=CAN_FILTER_ISOTP_RX, subaddr=None, bs=1, st=20)
     finally:
       signal.alarm(0)
 
     return ret
 
 class CanFilter(object):
-
   @staticmethod
   def reset(panda, enter_bootstub=False, enter_bootloader=False):
-    # reset
-    panda.set_safety_mode(Panda.SAFETY_ALLOUTPUT)
+    print(f"reset: bootstub {enter_bootstub}, bootloader {enter_bootloader}")
+    panda.set_safety_mode(Panda.SAFETY_ALLOUTPUT, True)
     if enter_bootloader:
       panda.can_send(CAN_FILTER_ADDR_MAGIC, b"\xce\xfa\xad\xde\x1e\x0b\xb0\x02", 0)
     else:
       if enter_bootstub:
         panda.can_send(CAN_FILTER_ADDR_MAGIC, b"\xce\xfa\xad\xde\x1e\x0b\xb0\x0a", 0)
       else:
+        # reset only
         panda.can_send(CAN_FILTER_ADDR_MAGIC, b"\xce\xfa\xad\xde\x1e\x0b\xb0\x01", 0)
 
   @staticmethod
   def flash(panda):
+    # set heartbeat_engaged=True, all controls_allowed will be set to false in 3 seconds
+    panda.send_heartbeat(True)
+
+    # set alloutput, disble heartbeat
+    panda.set_safety_mode(Panda.SAFETY_ALLOUTPUT, True)
+
     CanFilter.reset(panda, enter_bootstub=True)
 
-    time.sleep(0.2)
-    code = open(DEFAULT_FW_FN, "rb").read()
-
-    handle = CanHandle(panda, 0)
+    handle = BootLoaderHandle(panda)
     # confirm flasher is present
     print("flash: check flasher is present")
-    panda.set_safety_mode(Panda.SAFETY_ALLOUTPUT)
     fr = handle.controlRead(Panda.REQUEST_IN, 0xb0, 0, 0, 0xc)
     assert fr[4:8] == b"\xde\xad\xd0\x0d"
-    panda.send_heartbeat()
 
     # unlock flash
     print("flash: unlocking")
     handle.controlWrite(Panda.REQUEST_IN, 0xb1, 0, 0, b'', timeout=2)
-    panda.send_heartbeat()
 
     # erase size
     print("flash: erasing")
+    code = open(DEFAULT_FW_FN, "rb").read()
     handle.controlWrite(Panda.REQUEST_IN, 0xb3, len(code), 0, b'', timeout=10)
-    panda.send_heartbeat()
 
     # flash over EP2
     STEP = 0x10
@@ -89,13 +125,11 @@ class CanFilter(object):
       pct = 100 * i // len(code)
       sys.stdout.write(f"\rflash: {pct}% ")
       handle.bulkWrite(2, code[i:i + STEP], timeout=5)
-      panda.send_heartbeat()
     sys.stdout.write("\rflash: 100%\n")
 
     # lock flash
     print("flash: locking")
     handle.controlWrite(Panda.REQUEST_IN, 0xb4, 0, 0, b'', timeout=2)
-    panda.send_heartbeat()
 
     # reset
     print("flash: resetting")
@@ -112,35 +146,35 @@ class CanFilter(object):
 
   @staticmethod
   def get_log(panda):
-    panda.set_safety_mode(Panda.SAFETY_ALLOUTPUT)
-    ch = CanFilterHandle(panda, 0)
+    ch = FilterHandle(panda)
     dat = b'\x00'
+    panda.set_safety_mode(Panda.SAFETY_ALLOUTPUT, True)
     return ch.transact(dat)
 
   @staticmethod
   def get_signature(panda):
-    panda.set_safety_mode(Panda.SAFETY_ALLOUTPUT)
-    ch = CanFilterHandle(panda, 0)
+    ch = FilterHandle(panda)
     dat = b'\x01'
+    panda.set_safety_mode(Panda.SAFETY_ALLOUTPUT, True)
     return ch.transact(dat)
 
   @staticmethod
   def get_state(panda):
-    panda.set_safety_mode(Panda.SAFETY_ALLOUTPUT)
-    ch = CanFilterHandle(panda, 0)
+    ch = FilterHandle(panda)
     dat = b'\x02'
+    panda.set_safety_mode(Panda.SAFETY_ALLOUTPUT, True)
     st = ch.transact(dat)
     fmt = [
-      ('H', 'uptime'),
+      ('B', 'version'),
       ('B', 'crash_state'),
-      ('i', 'can1_state'),
-      ('i', 'can2_state'),
+      ('H', 'features'),
+
+      ('4s', 'git_version'),
+      ('I', 'uptime'),
       ('I', 'acc_ctrl_timeout'),
       ('I', 'aeb_timeout'),
       ('I', 'pre_colli_timeout'),
       ('I', 'pre_colli_2_timeout'),
-      ('4s', 'git_version'),
-      ('H', 'features'),
       ('I', 'can_rx_errs'),
       ('I', 'can_send_errs'),
       ('I', 'can_rx_cnt'),
@@ -148,25 +182,31 @@ class CanFilter(object):
       ('I', 'can_txd_cnt'),
       ('I', 'can_err_cnt'),
       ('I', 'can_overflow_cnt'),
+
+      ('B', 'can1_state'),
+      ('B', 'can2_state'),
+      ('x', 'pad'),
+      ('x', 'pad'),
     ]
 
-    f = ''
+    # little endian with no padding
+    f = '<'
     for x in fmt:
       f = f + x[0]
     r = struct.unpack(f, st)
 
     i = 0
     d = {}
-    while i < len(fmt):
-      d[fmt[i][1]] = r[i]
-      i =+ 1
+    for x in fmt:
+      if x[0] != 'x':
+        d[x[1]] = r[i]
+        i =+ 1
 
     return d
 
   @staticmethod
   def set_features(panda, features, save):
-    panda.set_safety_mode(Panda.SAFETY_ALLOUTPUT)
     v = struct.pack('>BH', save, features)
     d = b"\xce\xfa\xad\xde\x1f" + v
-    p.can_send(CAN_FILTER_ADDR_MAGIC, d, 0)
+    panda.can_send(CAN_FILTER_ADDR_MAGIC, d, 0)
 
